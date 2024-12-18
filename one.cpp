0d8f7fd45d08274f552d161f6c4c0ffe3448ae78
Ahmad Ansari <ahmadasghar20007@gmail.com>
2024-12-18 19:09:49 +0500

    Write a new grading program

--- one.cpp
+++ one.cpp
@@ -3,21 +3,27 @@ using namespace std;
 
 int main()
 {
-    // this is simple calculator program
-    int a,b;
-    cout<<"Enter first  number" <<endl;
-    cin>>a;
-    cout<<"Enter secound number"<<endl;
-    cin>>b;
+    // this is Grading marks program
+    int marks;
+    cout<<"Enter your marks"<<endl;
+    cin>>marks;
     
-    cout<<a+b<<endl;
+    if((marks>=80) && (marks<=100))
+    cout<<"Your Grade is A+"<<endl;
+    
+    else if((marks<80) && (marks>=60))
+    cout<<"your Grade is B+"<<endl;
 
-    cout<<a-b<<endl;
+    else if((marks<60) && (marks>=40))
+    cout<<"your Grade is C"<<endl;
 
-    cout<<a*b<<endl;
+    else if((marks<40) && (marks>=20))
+    cout<<"your Grade is E"<<endl;
 
-    cout<<a/b<<endl;
+    else if((marks<20) && (marks>=0))
+    cout<<"your grade is F"<<endl;
 
-    cout<<a%b<<endl;
+    else 
+    cout<<"Enter ivaild marks"<<endl;
     return 0;
 }
